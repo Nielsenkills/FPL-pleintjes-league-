91b11c2382aa81325568326c19e5683ff682bf9f
svper <verhulstrobin@gmail.com>
2015-08-11 16:27:44 +0200

    changes to css

--- src/app/transferschedule/transferschedule.controller.js
+++ src/app/transferschedule/transferschedule.controller.js
@@ -3,6 +3,7 @@
 angular.module('gretel')
     .controller('TransferScheduleCtrl', ['DataService',function(DataService) {
         var vm = this;
+        vm.currentUser = JSON.parse(localStorage.getItem('currentPlayer'));
         
         DataService.getAllTransferTimes().then(function(data) {
         	vm.transfertimes = data;
